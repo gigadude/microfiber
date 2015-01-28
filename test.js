130b9369ac4bb0ad8d180b10e740445b924207b7
gigadude <gigadude@users.noreply.github.com>
2015-01-28 09:27:48 -0800

    Create test.js

---
+++ test.js
@@ -0,0 +1,46 @@
+//
+// test.js - microfiber tests
+//
+
+'use strict';
+
+let util = require('util');
+let Scheduler = require('./microfiber');
+
+function somethingAsync(resolve, reject) {
+	let delay = 500 + Math.random() * 1000;
+	setTimeout(function done() {
+		console.log('async done!');
+		resolve(delay);
+	}, delay);
+}
+
+function* myWork(i) {
+	while (i-- > 0) {
+		console.log('step', i);
+		yield new Promise(somethingAsync);
+	}
+	console.log('done!');
+}
+
+function* master() {
+	console.log('starting master fibers...');
+	var scheduler = new Scheduler();
+	scheduler.add(myWork(3), 'sub job 1');
+	scheduler.add(myWork(3), 'sub job 2');
+	yield scheduler.run();
+}
+
+console.log('starting fibers...');
+var scheduler = new Scheduler();
+scheduler.add(myWork(5), 'myWork');
+scheduler.add(myWork(5), 'myWork');
+scheduler.add(master(), 'master');
+scheduler.run().then(
+	function pass(val) {
+		console.log('ran', val, 'tasks');
+	},
+	function fail(err) {
+		console.log('error in tasks:', err);
+	}
+);
